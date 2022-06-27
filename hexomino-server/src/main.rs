99a62c67e6d54bae6bfad749d4d977695692b1ea
Cheng-Min Chiang <chmnchiang@gmail.com>
2022-06-27 00:27:31 -0700

    Add file log

--- hexomino-server/src/main.rs
+++ hexomino-server/src/main.rs
@@ -4,13 +4,16 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 #[tokio::main]
 async fn main() {
     let _ = dotenv::dotenv();
+    let file_appender = tracing_appender::rolling::hourly("./logs", "server.log");
+    let (file_writer, _guard) = tracing_appender::non_blocking(file_appender);
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG")
                 .unwrap_or_else(|_| "hexomino_server=debug,tower_http=debug".into()),
         ))
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer().with_writer(file_writer))
+        .with(tracing_subscriber::fmt::layer().with_writer(std::io::stdout))
         .init();
     axum::Server::bind(
         &std::env::var("SERVER_ADDR")

038e16b57257c1d2bb971b8951752920f1c0c073
Cheng-Min Chiang <chmnchiang@gmail.com>
2022-07-02 02:25:40 -0700

    Add file log level

--- hexomino-server/src/main.rs
+++ hexomino-server/src/main.rs
@@ -1,18 +1,28 @@
 use hexomino_server::make_app;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{
+    filter::LevelFilter, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer,
+};
 
 #[tokio::main]
 async fn main() {
     let _ = dotenv::dotenv();
     let file_appender = tracing_appender::rolling::hourly("./logs", "server.log");
     let (file_writer, _guard) = tracing_appender::non_blocking(file_appender);
+    let file_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .with_env_var("FILE_LOG_LEVEL")
+        .from_env_lossy();
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG")
                 .unwrap_or_else(|_| "hexomino_server=debug,tower_http=debug".into()),
         ))
-        .with(tracing_subscriber::fmt::layer().with_writer(file_writer))
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_writer(file_writer)
+                .with_filter(file_filter),
+        )
         .with(tracing_subscriber::fmt::layer().with_writer(std::io::stdout))
         .init();
     axum::Server::bind(
